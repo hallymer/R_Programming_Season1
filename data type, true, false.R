5df3462ebe55686288af4691e7425bf9df935d17
hallymer <mu072468@gmail.com>
2021-01-05 00:12:32 +0900

    [Insert] R Programming (2021.01.05)
    Finish about data type, true, false
    
    x*y를 실행시키면 오류가 발생한다.
    why? x는 numeric이고 y는 character(=string)이기 때문
    
    이번 강의에서 str() 함수를 기억하자!
    str() 함수는 테이블의 구조, 데이터의 타입을 볼 수 있는 함수이다.
    
    1+2 = 3 여기서 = 표시는 같다는 의미가 아니다. "담겠다"라는 의미이다. 해석하면 1+2의 값을 3에 담겠다는 의미이다. 그래서 Error가 뜬다.
    같다는 의미는 == 이렇게 작성해줘야 한다.
    또한, 숫자는 Values의 name이 될 수 없다.

---
+++ data type, true, false.R
@@ -0,0 +1,23 @@
+x <- c(1,2,3,4,6)
+y <- c('1','2','3','4','5')
+z <- c('how','are', 'you', '?')
+
+length(x)
+length(y)
+x*y
+
+z
+x
+y
+
+str(x)
+str(y)
+str(z)
+
+1+2=3 #Error
+1+2==3 #TRUE
+1+5==7 #FALSE
+
+x #1 2 3 4 6
+y #"1" "2" "3" "4" "5"
+x==y #TRUE  TRUE  TRUE  TRUE  FALSE
