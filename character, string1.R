70892fa98c718cc3b46d5c3bccf684ded3d78471
hallymer <mu072468@gmail.com>
2021-01-11 23:57:54 +0900

    [Insert] R Programming (2021.01.11)
    Finish about character, string1
    
    x <- 'what's your name?'를 실행하면 에러가 뜬다
    그 이유는 작은따옴표를 사용해서 에러가 뜨는것 이럴때는 큰따옴표를 사용하면 해결된다.
    
    y[12] <- "twelve"
    y
    실행을 시킨다음 콘솔창을 보면 주목할 부분은 NA(결측치)가 들어가있다.
    결측치 안에 데이터를 넣을 수 있다.

---
+++ character, string1.R
@@ -0,0 +1,56 @@
+x <- 'what is your name?'
+x <- "what is your name?"
+#x <- 'what's your name?' Error
+x <- "what's your name?"
+x
+
+y <- character()
+y2 <- ""
+y
+y2
+class(y)
+class(y2)
+length(y)
+length(y2)
+
+y <- c("e", "12", "2")
+y
+y <- character(10)
+y
+y[3] <- "third"
+y
+y[12] <- "twelve"
+y
+length(y)
+#주목할 부분은 NA(결측치)가 들어가있다. 결측치 안에 데이터를 넣을 수 있다.
+y[11] <- 11
+y
+#----------------------fundamental contents----------------------
+n = 3
+m = "3"
+is.character(n)
+is.character(m)
+
+class(n)
+class(m)
+
+n2 <- as.character(n) #형변환
+class(n2)
+
+t <- c(1:5)
+t2 <- c(1:5, "a")
+t3 <- c(1:4, TRUE, FALSE)
+t
+t2
+t3
+
+class(t)
+class(t2)
+class(t3)
+
+t4 <- c(1:4, TRUE, FALSE, "a")
+t4
+class(t4)
+
+df1 <- data.frame(n=c(1:5), letters=c("a","b","23","1","4"))
+str(df1)
