f63394c33514ff638b9040909b17d7a9c10e295c
hallymer <mu072468@gmail.com>
2021-01-19 17:57:09 +0900

    [Insert] R Programming (2021.01.19)
    Finish about 문자, 문자열, character, string3
    
    grep() 함수는 문자열 vector에서 해당 패턴을 만족하는 index를 찾아주는 함수
    stringr패키지는 문자열을 다루고 조작하는 패키지
    str_count()메소드는 stringr패키지 안에 메소드, 정규식에 맞는 문자열이 총 몇개 존재하는지 반환하는 메소드

--- 14. 문자, 문자열, character, string3.R
+++ 14. 문자, 문자열, character, string3.R
@@ -6,8 +6,17 @@ nchar(cars)
 cars[which(nchar(cars)==max(nchar(cars)))]
 
 grep("z", cars)
+grep("z", cars, value = TRUE)
+grep("rd", cars, value = TRUE)
+grep("[vV]", cars, value = TRUE)
+grep("t", cars, value = TRUE)
+grep("t", tolower(cars), value = TRUE)
+grep("toyota", cars, value=TRUE)
+grep("toyota", tolower(cars), value=TRUE)
 grep("TOYOTA", toupper(cars), value=TRUE)
 
 library(stringr)
 cars
+str_count(cars,"t")
+str_count(cars,"o")
 sum(str_count(toupper(cars), "TOYOTA"))
